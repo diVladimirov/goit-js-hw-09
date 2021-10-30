ee8292c52f1c5e6dbcbf683a65dd917dad1c213d
Vadim Vladimirov <kurtonchik@gmail.com>
2021-10-30 22:38:38 +0300

    done task01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
+let timerId = null;
 
+refs.btnStart.addEventListener('click', OnBtnStartClick);
+refs.btnStop.addEventListener('click', OnBtnStopClick);
+
+function OnBtnStartClick() {
+  refs.btnStart.setAttribute('disabled', 'disabled');
+
+  timerId = setInterval(() => {
+    let color = getRandomHexColor();
+    document.body.style.backgroundColor = color;
+  }, 1000);
+}
+
+function OnBtnStopClick() {
+  clearInterval(timerId);
+  refs.btnStart.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
